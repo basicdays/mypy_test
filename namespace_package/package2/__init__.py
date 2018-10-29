3c3e74b1774a5a6426e2e02376a12a7e3be4ab4b
basicdays <info@basicdays.com>
2018-10-29 14:37:48 -0500

    Test re-exports

--- namespace_package/package2/__init__.py
+++ namespace_package/package2/__init__.py
@@ -1,6 +1,7 @@
-# from namespace_package.package1 import add_stuff
+from namespace_package.package1 import add_stuff
 # from namespace_package.package1.module_a import add_stuff
-from namespace_package.package1.subpackage1 import add_stuff
+# from namespace_package.package1.module_b import add_stuff
+# from namespace_package.package1.subpackage1 import add_stuff
 
 
 def doit() -> int:
