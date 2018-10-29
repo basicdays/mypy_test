7892b80682654f5f19ab21ee0051750d519d446e
basicdays <info@basicdays.com>
2018-10-29 14:26:56 -0500

    Figured out exactly what is not working

--- namespace_package/package1/__init__.py
+++ namespace_package/package1/__init__.py
@@ -1,2 +1,6 @@
 # from .module_a import add_stuff
-from namespace_package.package1.module_a import add_stuff
+# from namespace_package.package1.module_a import add_stuff
+
+
+def add_stuff(part1: int, part2: int) -> int:
+    return part1 + part2
--- namespace_package/package2/__init__.py
+++ namespace_package/package2/__init__.py
@@ -1,4 +1,6 @@
-from namespace_package.package1 import add_stuff
+# from namespace_package.package1 import add_stuff
+# from namespace_package.package1.module_a import add_stuff
+from namespace_package.package1.subpackage1 import add_stuff
 
 
 def doit() -> int:
